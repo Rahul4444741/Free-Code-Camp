3fd2e40547d2c65ae0b63b7a578a159df40e478d
Rahul4444741 <77558879+Rahul4444741@users.noreply.github.com>
2021-06-24 12:41:35 +0530

    Create UsingObjectsForLookups.js

---
+++ UsingObjectsForLookups.js
@@ -0,0 +1,17 @@
+// Setup
+function phoneticLookup(val) {
+    var lookup = {
+      "alpha" : "Adams",
+      "bravo" : "Boston",
+      "charlie" : "Chicago",
+      "delta" : "Denver",
+      "echo" : "Easy",
+      "foxtrot" : "Frank"
+    };
+  
+    return lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
